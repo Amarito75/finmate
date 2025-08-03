08f735d7dcb46955ed8db8b09014b037cff5e895
Amar Matallah <a.matallah767@gmail.com>
2025-08-04 00:12:08 +0200

    :lipstick: add chat page

---
+++ apps/web/src/app/dashboard/chat/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChatInput } from "./_components/chat-input";
+
+const ChatPage = () => {
+  return (
+    <div className="">
+      <ChatInput />
+    </div>
+  );
+};
+
+export default ChatPage;
--- apps/web/src/components/app-sidebar.tsx
+++ apps/web/src/components/app-sidebar.tsx
@@ -35,7 +35,7 @@ const items = [
   },
   {
     title: "Chat",
-    url: "#",
+    url: "/dashboard/chat",
     icon: MessageCircle,
   },
 ];
